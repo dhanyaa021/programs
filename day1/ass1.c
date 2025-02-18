87ded56cc2309aa57c9defa0c2e83f44697c13cb
dhanyaa021 <2023beai046@mite.ac.in>
2025-02-18 09:35:22 +0530

    Update ass1.c

--- day1/ass1.c
+++ day1/ass1.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+//function to raed applicant deatils
+
 void read_details(float* p_salary,int* p_score,int* p_experience){
     printf("Enter salary:");
     scanf("%f",p_salary);
@@ -9,16 +11,21 @@ void read_details(float* p_salary,int* p_score,int* p_experience){
     printf("Enter experience:");
     scanf("%d",p_experience);
 }
-
+//function to check loan eligibility
 int isEligible(float salary,int score,int experience){
     return((salary>=30000)&&(score>=750)&&(experience>=2));
 }
+//main function begins here
 int main()
 {
     float salary;
     int score;
     int experience;
+    
+    //read applicant details
     read_details(&salary,&score,&experience);
+    
+    //check loan eligibility
     if(isEligible(salary,score,experince)){
         printf("Loan approved");
     }
